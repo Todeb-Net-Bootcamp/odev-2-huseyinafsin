04f56b7ebaecaf681121c5326946ad9635e24ddc
huseyinafsin <huseyinafsin4@gmail.com>
2022-07-05 22:35:59 +0300

    conflitc

---
+++ TodebOdev2/Abstracts/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodebOdev2.Abstracts
+{
+    public abstract class  Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public virtual void Area()
+        {
+        }
+
+    }
+}
---
+++ TodebOdev2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TodebOdev2.Abstracts;
+using TodebOdev2.Concrete;
+public class Program
+{
+    public static void Main()
+    {
+        Console.BackgroundColor = ConsoleColor.Black;
+
+        Rectangle rectangle  = new Rectangle();
+        rectangle.Width = 5;
+        rectangle.Height = 4;
+        rectangle.Area();
+
+        Triangle triangle = new Triangle();
+        triangle.Width = 5;
+        triangle.Height = 4;
+        triangle.Area();
+
+        Console.ForegroundColor= ConsoleColor.White;
+    }
+}
+
